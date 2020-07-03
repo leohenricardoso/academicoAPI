eea5dd4de1ab3be4321d4845cf4b03fe03460d95
Pedro Diniz <pedro_diniz_l2@hotmail.com>
2020-07-02 21:10:19 -0300

    :construction: Controller & routes -> BusinessContact

--- app/Controllers/Http/BusinessContactController.js
+++ app/Controllers/Http/BusinessContactController.js
@@ -4,7 +4,7 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
-const Contact = use('App/Models/BusinessContact')
+const BusinessContact = use('App/Models/BusinessContact')
 
 /**
  * Resourceful controller for interacting with businesscontacts
@@ -19,12 +19,12 @@ class BusinessContactController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view, auth }) {
+  async index ({  response, auth }) {
     if(!auth.user.id) {
       return response.status(401)
     }
 
-    return await Contact.all()
+    return await BusinessContact.all()
   }
 
   /**
@@ -35,7 +35,23 @@ class BusinessContactController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store ({ request, response, auth }) {
+    if(!auth.user.id) {
+      return response.status(401)
+    }
+
+    const data = request.only([
+      'name',
+      'cpf',
+      'crp',
+      'email',
+      'subject',
+      'message'
+    ])
+
+    const businesscontact = await BusinessContact.create({
+      ...data
+    })
   }
 
   /**
@@ -47,7 +63,12 @@ class BusinessContactController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params, response, auth }) {
+    if(!auth.user.id) {
+      return response.status(401)
+    }
+
+    return await BusinessContact.findOrFail(params.id)
   }
 
   /**
@@ -58,7 +79,25 @@ class BusinessContactController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params, request, response, auth }) {
+    if(!auth.user.id) {
+      return response.status(401)
+    }
+
+    const businesscontact = await BusinessContact.findOrFail(params.id)
+    const data = request.only([
+      'name',
+      'cpf',
+      'crp',
+      'email',
+      'subject',
+      'message'
+    ])
+
+    businesscontact.merge(data)
+    await businesscontact.save()
+
+    return businesscontact
   }
 
   /**
@@ -69,7 +108,12 @@ class BusinessContactController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params, response, auth }) {
+    if(!auth.user.id) {
+      return response.status(401)
+    }
+    const businesscontact = await BusinessContact.findOrFail(params.id)
+    await businesscontact.delete()
   }
 }
 
