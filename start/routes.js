3b3cb11188755221d41a6f5ca02a91827f9a0e58
Pedro Diniz <pedro_diniz_l2@hotmail.com>
2020-07-08 22:09:38 -0300

    :wrench: Config das rotas

--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,51 @@ const Route = use('Route')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+Route.group(() => {
+  Route.resource('api/course-category', 'CourseCategoryController').apiOnly()
+}).middleware('auth')
+
+
+Route.group(() => {
+  Route.resource('api/newsletter', 'NewsletterController').apiOnly()
+}).middleware('auth')
+
+Route.group(() => {
+  Route.resource('api/business-contact', 'BusinessContactController').apiOnly()
+}).middleware('auth')
+
+Route.group(() => {
+  Route.resource('api/speaker', 'CourseSpeakerController').apiOnly()
+}).middleware('auth')
+
+Route.group(() => {
+  Route.resource('api/contact', 'ContactController').apiOnly()
+}).middleware('auth')
+
+Route.group(() => {
+  Route.resource('api/paymentpagarme', 'PaymentPagarmeController').apiOnly()
+}).middleware('auth')
+
+Route.group(() => {
+  Route.resource('api/course', 'CourseController').apiOnly()
+}).middleware('auth')
+
+Route.group(() => {
+  Route.resource('api/course-type', 'CourseTypeController').apiOnly()
+}).middleware('auth')
+
+Route.group(() => {
+  Route.resource('api/student', 'StudentController').apiOnly()
+}).middleware('auth')
+
+Route.group(() => {
+  Route.resource('api/my-course', 'MyCourseController').apiOnly()
+}).middleware('auth')
+
+Route.get('/api/courses-category/:category_id', 'CourseController.getCoursesByCategoryId').middleware(['auth'])
+Route.get('/api/courses-search/:name', 'CourseController.getCoursesByName').middleware(['auth'])
+Route.get('/api/courses-type/:type_id', 'CourseController.getCoursesByTypeId').middleware(['auth'])
+Route.get('/api/courses-speaker/:speaker_id', 'CourseController.getCoursesBySpeakerId').middleware(['auth'])
+Route.get('/api/courses-price/:price_min/:price_max', 'CourseController.getCoursesByPrice').middleware(['auth'])
+
