f686f170afc7191b059abe22300afaab184c8c33
Leonardo Henrique <leohenricardoso@gmail.com>
2020-07-20 19:15:04 -0300

    Ajuste rotas duplicadas

--- start/routes.js
+++ start/routes.js
@@ -86,7 +86,6 @@ Route.get('/api/name-asc', 'CourseController.getNameAsc').middleware(['auth'])
 Route.get('/api/name-desc', 'CourseController.getNameDesc').middleware(['auth'])
 Route.post('/api/courses-image/:id', 'CourseController.saveImage').middleware(['auth'])
 Route.post('/api/send-contact-email', 'SendEmailController.sendContactEmail').middleware(['auth'])
-Route.post('/api/send-contact-email', 'SendEmailController.sendContactEmail').middleware(['auth'])
 Route.post('/api/send-buy-email/:courseId', 'SendEmailController.sendShopEmail').middleware(['auth'])
 Route.get('/api/send-course-invite-qrcode/:courseId/:studentId', 'SendEmailController.sendInviteToPresentialCourse').middleware(['auth'])
 
