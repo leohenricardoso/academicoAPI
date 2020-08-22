70f68e9789bb22b5eaf7e4062a9166d7a8352bda
Leonardo Henrique <leohenricardoso@gmail.com>
2020-08-22 11:48:33 -0300

    :construction: Muda funcionalidade de salvar dados de pagamento para o postback

--- app/Controllers/Http/MercadoPagoController.js
+++ app/Controllers/Http/MercadoPagoController.js
@@ -65,17 +65,15 @@ class MercadoPagoController {
         payer: {
           first_name: req.data.name
         },
-        items:[
-          {
-            id: courseId,
-            title: course.name,
-            description: course.description,
-            picture_url: `${Env.get('URL_COURSE_IMG')}${course.image_path}`,
-            category_id: courseCategoryLabel,
-            quantity: 1,
-            unit_price: req.data.amount
-          }
-        ]
+        items: [{
+          id: courseId,
+          title: course.name,
+          description: course.description,
+          picture_url: `${Env.get('URL_COURSE_IMG')}${course.image_path}`,
+          category_id: courseCategoryLabel,
+          quantity: 1,
+          unit_price: req.data.amount
+        }]
       }
     }
 
@@ -96,33 +94,7 @@ class MercadoPagoController {
       return null
     }
 
-    // Salva dados de pagamento no banco de dados
-    const jsonData = JSON.stringify(paymentReturn)
-    const mercadopago_model = await MercadoPagoModel.create({
-      course_id: courseId,
-      student_id: student.id,
-      transaction_id: paymentReturn.id,
-      status: paymentReturn.status,
-      payment_method_id: paymentReturn.payment_method_id,
-      payment_type_id: paymentReturn.payment_type_id,
-      transaction_amount: paymentReturn.transaction_amount,
-      net_received_amount: paymentReturn.net_received_amount,
-      total_paid_amount: paymentReturn.total_paid_amount,
-      overpaid_amount: paymentReturn.overpaid_amount,
-      installment_amount: paymentReturn.installment_amount,
-      transaction_amount_refunded: paymentReturn.transaction_amount_refunded,
-      total_fee_amount: paymentReturn.total_fee_amount,
-      captured: paymentReturn.captured,
-      payer_doc: paymentReturn.payer_doc,
-      notification_url: paymentReturn.notification_url,
-      installments: paymentReturn.installments,
-      date_created: paymentReturn.date_created,
-      date_approved: paymentReturn.date_approved,
-      date_last_updated: paymentReturn.date_last_updated,
-      data: jsonData
-    })
-
-    return mercadopago_model
+    return paymentReturn
   }
 
   /**
@@ -138,30 +110,69 @@ class MercadoPagoController {
     response,
     request
   }) {
-    const req = request.all()
+    try {
+      const req = request.all()
 
-    // Set access token do MERCADOPAGO
-    MERCADOPAGO.configure({
-      access_token: Env.get('ACCESS_KEY_MP')
-    })
+      // Set access token do MERCADOPAGO
+      MERCADOPAGO.configure({
+        access_token: Env.get('ACCESS_KEY_MP')
+      })
 
-    const paymentPostback = await MERCADOPAGO.payment.get(req.data.id)
-    Logger.info('POSTBACK')
-    Logger.info(paymentPostback)
+      const paymentPostback = await MERCADOPAGO.payment.get(req.data.id)
+      Logger.info('POSTBACK')
+      Logger.info(paymentPostback.response)
 
-    await this.sleep(10000)
+      const payment = await MercadoPagoModel.findBy('transaction_id', req.data.id)
 
-    const payment = await MercadoPagoModel.findBy('transaction_id', req.data.id)
-    Logger.info('ENCONTRADO')
-    Logger.info(payment)
+      if (payment) {
+        // Caso o status do pagamento for diferente do salvo no banco, irá atualizar as
+        // informações no banco de dados
+        if (payment.status != paymentPostback.response.status) {
+          // Pega dados a ser mergeados
+          let newPaymentStatus = {
+            status: paymentPostback.response.status,
+            date_approved: paymentPostback.response.date_approved,
+            date_last_updated: paymentPostback.response.date_last_updated,
+            data: paymentPostback.response
+          }
+          // Mergeia e salva os dados no banco de dados
+          payment.merge(newPaymentStatus)
+          await payment.save()
+        }
+      } else {
+        // Salva dados de pagamento no banco de dados
+        const jsonData = JSON.stringify(paymentPostback.response)
+        const mercadopago_model = await MercadoPagoModel.create({
+          course_id: courseId,
+          student_id: student.id,
+          transaction_id: paymentReturn.id,
+          status: paymentReturn.status,
+          payment_method_id: paymentReturn.payment_method_id,
+          payment_type_id: paymentReturn.payment_type_id,
+          transaction_amount: paymentReturn.transaction_amount,
+          net_received_amount: paymentReturn.net_received_amount,
+          total_paid_amount: paymentReturn.total_paid_amount,
+          overpaid_amount: paymentReturn.overpaid_amount,
+          installment_amount: paymentReturn.installment_amount,
+          transaction_amount_refunded: paymentReturn.transaction_amount_refunded,
+          total_fee_amount: paymentReturn.total_fee_amount,
+          captured: paymentReturn.captured,
+          payer_doc: paymentReturn.payer_doc,
+          notification_url: paymentReturn.notification_url,
+          installments: paymentReturn.installments,
+          date_created: paymentReturn.date_created,
+          date_approved: paymentReturn.date_approved,
+          date_last_updated: paymentReturn.date_last_updated,
+          data: jsonData
+        })
+      }
 
+      return response.status(200)
+
+    } catch (error) {
+      Logger.error(error)
+    }
   }
-
-  function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-    })
-  }   
 
   /**
    * Show a list of all mercadopagos.
@@ -271,26 +282,24 @@ class MercadoPagoController {
     });
     const req = request.post()
     let preference = {
-      items: [
-        {
-          title: req.course,
-          unit_price: req.amount,
-          quantity: 1,
-        }
-      ]
+      items: [{
+        title: req.course,
+        unit_price: req.amount,
+        quantity: 1,
+      }]
     };
     var teste = {
       teste2: 'blabla'
     }
     var data = {}
     await MERCADOPAGO.preferences.create(preference)
-      .then(function(res){
+      .then(function (res) {
         data.result = res.body
-      }).catch(function(error){
+      }).catch(function (error) {
         data.result = false
       });
 
-      return data
+    return data
   }
 }
 
