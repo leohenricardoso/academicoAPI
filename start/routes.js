68220728ae7d9803146a1187ea1ce544adbb749f
Leonardo Henrique <leohenricardoso@gmail.com>
2020-09-20 12:26:20 -0300

    Ajusta middleware para auth:admin

--- start/routes.js
+++ start/routes.js
@@ -31,7 +31,7 @@ Route.post('/api/course-category', 'CourseCategoryController.store').middleware(
 Route.put('/api/course-category/:id', 'CourseCategoryController.update').middleware('auth:admin')   // Admin
 Route.delete('/api/course-category/:id', 'CourseCategoryController.destroy').middleware('auth:admin')   // Admin
 
-Route.post('/api/search-email/:pages/:limit', 'StudentController.searchStudent').middleware(['auth'])   // Admin
+Route.post('/api/search-email/:pages/:limit', 'StudentController.searchStudent').middleware('auth:admin')  // Admin
 
 /** Newsletter */
 Route.post('/api/newsletter', 'NewsletterController.store')   // Free
